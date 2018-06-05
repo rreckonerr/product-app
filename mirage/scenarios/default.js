398affd43807e39ce105d63bddd520769cdfb1d5
rreckonerr <rreckonerr@gmail.com>
2018-06-05 16:58:09 +0700

    installed and added mirage and faker

---
+++ mirage/scenarios/default.js
@@ -0,0 +1,13 @@
+export default function(server) {
+
+  /*
+    Seed your development database using your factories.
+    This data will not be loaded in your tests.
+  */
+
+  server.createList('category', 10);
+
+  // this.get('/categories');
+  // this.post('/categories');
+  
+}
